eb74a77638bc36824d66f0441fb28522e97c1e15
AyushKT21 <ayushtiwari20072001@gmail.com>
2022-11-11 11:07:33 +0530

    awesome

---
+++ wp-admin/post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=httplocalhost/wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "httplocalhost/wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="httplocalhost/wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1">httplocalhost/wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fpost.php&reauth=1</a></p>
+	</body>
+</html>
